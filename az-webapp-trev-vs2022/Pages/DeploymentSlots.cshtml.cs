1d389a68d53a70052d902e92251354c3cf3e5c4f
Andre Steinmetz <andre_steinmetz@web.de>
2024-01-26 09:25:49 +0100

    added changes for changing

---
+++ az-webapp-trev-vs2022/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az_webapp_trev_vs2022.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- az-webapp-trev-vs2022/Pages/Privacy.cshtml.cs
+++ az-webapp-trev-vs2022/Pages/Privacy.cshtml.cs
@@ -14,7 +14,6 @@ namespace az_webapp_trev_vs2022.Pages
 
         public void OnGet()
         {
-            throw new Exception("Testing Insights");
         }
     }
 
