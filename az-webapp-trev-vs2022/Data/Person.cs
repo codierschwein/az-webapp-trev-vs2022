c2fab9ea8ae35727b664a05a75e606f63ece95b2
Andre Steinmetz <andre_steinmetz@web.de>
2024-01-29 09:58:08 +0100

    First Commit

---
+++ az-webapp-trev-vs2022/Data/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace az_webapp_trev_vs2022.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ az-webapp-trev-vs2022/Pages/Persons/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using az_webapp_trev_vs2022.Data;
+
+namespace az_webapp_trev_vs2022.Pages.Persons
+{
+    public class IndexModel : PageModel
+    {
+        private readonly az_webapp_trev_vs2022.Data.AppDbContext _context;
+
+        public IndexModel(az_webapp_trev_vs2022.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Persons.ToListAsync();
+        }
+    }
+}
